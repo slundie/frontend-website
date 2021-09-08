e29656a9ad6c62255e825e5141dc539d143a0742
Sam <hulklundie@gmail.com>
2021-09-09 08:33:50 +1000

    add google site verification meta tag to headMetadata.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     env: {
       "DEV_API_URL": "http://localhost:5000",
       "PRODUCTION_API_URL": "https://www.api.futurestate-it.com/",
-      "GOOGLE_VERIFICATION_ID": "googlea0348f1231d0b8e3.html"
+      "GOOGLE_VERIFICATION_ID": "VPVSZ9pHUWEmXaqOPblOaf9yiGNAgU0Re5nKm9EzsIg" 
     }
   }
