38a7d98e0b5a6f6e3f5f50f9e2b3077bb25a6f9b
Sam <hulklundie@gmail.com>
2021-09-10 21:01:26 +1000

    Add google analytics tracking code to each page now under slundie@techisolutions.com.au

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
       "DEV_API_URL": "http://localhost:5000",
       "PRODUCTION_API_URL": "https://www.api.futurestate-it.com/",
       "GOOGLE_VERIFICATION_ID": "VPVSZ9pHUWEmXaqOPblOaf9yiGNAgU0Re5nKm9EzsIg", 
-      "GOOGLE_ANALYTICS_ID": "G-L0ZHH79JBT"
+      "GOOGLE_ANALYTICS_ID": "G-36CR45Y74K"
     }
   }
