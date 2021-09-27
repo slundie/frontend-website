e7ccfd8db1a8fab1d31f153519292793571860dc
Sam <hulklundie@gmail.com>
2021-09-28 05:27:49 +1000

    fixed / in API URL

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
       "DEV_API_URL": "http://localhost:5000",
-      "PRODUCTION_API_URL": "https://www.api.futurestate-it.com/",
+      "PRODUCTION_API_URL": "https://www.api.futurestate-it.com",
       "GOOGLE_VERIFICATION_ID": "VPVSZ9pHUWEmXaqOPblOaf9yiGNAgU0Re5nKm9EzsIg", 
       "GOOGLE_ANALYTICS_ID": "G-36CR45Y74K"
     }
